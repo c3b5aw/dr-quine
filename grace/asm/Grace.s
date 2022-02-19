cc86c2348396ab23dbd6121bc52775d79841c5b9
c3b5aw <dev@c3b5aw.dev>
2022-02-19 15:43:25 +0100

    update GRACE to fit scale

--- grace/asm/Grace.s
+++ grace/asm/Grace.s
@@ -1,23 +1,23 @@
+; Comment
+%define MAIN		main:
+%define NIL			0
+%define ENTRYPOINT	global main
 section .data
-	source_code: db "section .data%2$c%3$csource_code: db %4$c%1$s%4$c, 0%2$c%3$ckid_name: db %4$cGrace_kid.s%4$c, 0%2$c%3$ckid_mode: db %4$cw%4$c, 0%2$c%3$cfptr dq 0%2$c%2$csection .text%2$c%3$cglobal main%2$c%2$c%3$cextern fprintf%2$c%3$cextern fopen%2$c%3$cextern fclose%2$c%2$cmain:%2$c%3$ccall grace%2$c%2$c%3$cmov eax, 0x0%2$c%3$cret%2$c%2$cgrace:%2$c%3$cmov rdi, kid_name%2$c%3$cmov rsi, kid_mode%2$c%3$ccall fopen%2$c%2$c%3$cmov [fptr], rax%2$c%2$c%3$cmov rdi, [fptr]%2$c%3$cmov rsi, source_code%2$c%3$cmov rdx, source_code%2$c%3$cmov rcx, 0x0A%2$c%3$cmov r8, 0x09%2$c%3$cmov r9, 0x22%2$c%3$ccall fprintf%2$c%2$c%3$cmov rdi, [fptr]%2$c%3$ccall fclose", 0
+	source_code: db "; Comment%2$c%%define MAIN%3$c%3$cmain:%2$c%%define NIL%3$c%3$c%3$c0%2$c%%define ENTRYPOINT%3$cglobal main%2$csection .data%2$c%3$csource_code: db %4$c%1$s%4$c, 0%2$c%3$ckid_name: db %4$cGrace_kid.s%4$c, 0%2$c%3$ckid_mode: db %4$cw%4$c, 0%2$c%3$cfptr dd NIL%2$c%2$csection .text%2$c%3$cENTRYPOINT%2$c%2$c%3$cextern fprintf%2$c%3$cextern fopen%2$c%3$cextern fclose%2$c%2$cMAIN%2$c%3$cpush rbx%2$c%2$c%3$cmov rdi, kid_name%2$c%3$cmov rsi, kid_mode%2$c%3$ccall fopen%2$c%2$c%3$cmov [fptr], rax%2$c%2$c%3$cmov rdi, [fptr]%2$c%3$cmov rsi, source_code%2$c%3$cmov rdx, source_code%2$c%3$cmov rcx, 0x0A%2$c%3$cmov r8, 0x09%2$c%3$cmov r9, 0x22%2$c%3$ccall fprintf%2$c%2$c%3$cmov rdi, [fptr]%2$c%3$ccall fclose%2$c%2$c%3$cmov eax, 0x0%2$c%3$cret", 0
 	kid_name: db "Grace_kid.s", 0
 	kid_mode: db "w", 0
-	fptr dq 0
+	fptr dd NIL
 
 section .text
-	global main
+	ENTRYPOINT
 
 	extern fprintf
 	extern fopen
 	extern fclose
 
-main:
-	call grace
+MAIN
+	push rbx
 
-	mov eax, 0x0
-	ret
-
-grace:
 	mov rdi, kid_name
 	mov rsi, kid_mode
 	call fopen
@@ -34,3 +34,6 @@ grace:
 
 	mov rdi, [fptr]
 	call fclose
+
+	mov eax, 0x0
+	ret
--- grace/go/Grace.go
+++ grace/go/Grace.go
@@ -6,8 +6,12 @@ import (
 	"os"
 )
 
+/*
+	Comment
+*/
+
 func main() {
-	s := "package main%[2]c%[2]cimport (%[2]c%[3]c%[4]cbufio%[4]c%[2]c%[3]c%[4]cfmt%[4]c%[2]c%[3]c%[4]cos%[4]c%[2]c)%[2]c%[2]cfunc main() {%[2]c%[3]cs := %[4]c%[1]s%[4]c%[2]c%[2]c%[3]cfptr, err := os.OpenFile(%[4]cGrace_kid.go%[4]c, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)%[2]c%[3]cif err != nil {%[2]c%[3]c%[3]cos.Exit(1)%[2]c%[3]c}%[2]c%[2]c%[3]cw := bufio.NewWriter(fptr)%[2]c%[3]cfmt.Fprintf(w, s, s, 10, 9, 34)%[2]c%[3]cw.Flush()%[2]c%[2]c%[3]cif err := fptr.Close(); err != nil {%[2]c%[3]c%[3]cos.Exit(1)%[2]c%[3]c}%[2]c}%[2]c"
+	s := "package main%[2]c%[2]cimport (%[2]c%[3]c%[4]cbufio%[4]c%[2]c%[3]c%[4]cfmt%[4]c%[2]c%[3]c%[4]cos%[4]c%[2]c)%[2]c%[2]c/*%[2]c%[3]cComment%[2]c*/%[2]c%[2]cfunc main() {%[2]c%[3]cs := %[4]c%[1]s%[4]c%[2]c%[2]c%[3]cfptr, err := os.OpenFile(%[4]cGrace_kid.go%[4]c, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)%[2]c%[3]cif err != nil {%[2]c%[3]c%[3]cos.Exit(1)%[2]c%[3]c}%[2]c%[2]c%[3]cw := bufio.NewWriter(fptr)%[2]c%[3]cfmt.Fprintf(w, s, s, 10, 9, 34)%[2]c%[3]cw.Flush()%[2]c%[2]c%[3]cif err := fptr.Close(); err != nil {%[2]c%[3]c%[3]cos.Exit(1)%[2]c%[3]c}%[2]c}%[2]c"
 
 	fptr, err := os.OpenFile("Grace_kid.go", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
