4f211100ccf19281cba7f9a0f016cbe0ea5c0e4a
c3b5aw <dev@c3b5aw.dev>
2022-02-22 09:04:14 +0100

    Sully [Go] fix

--- sully/go/Sully.go
+++ sully/go/Sully.go
@@ -24,7 +24,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		s := "package main%[3]c%[3]cimport (%[3]c%[4]c%[5]cbufio%[5]c%[3]c%[4]c%[5]cfmt%[5]c%[3]c%[4]c%[5]cos%[5]c%[3]c%[4]c%[5]cos/exec%[5]c%[3]c%[4]c%[5]cstrconv%[5]c%[3]c)%[3]c%[3]cfunc main() {%[3]c%[4]ci := %[2]d%[3]c%[3]c%[4]cif _, err := os.Stat(%[5]cSully_5.go%[5]c); err == nil {%[3]c%[4]c%[4]ci -= 1%[3]c%[4]c}%[3]c%[3]c%[4]cif i >= 0 {%[3]c%[4]c%[4]cfile_name := %[5]cSully_%[5]c + strconv.Itoa(i)%[3]c%[4]c%[4]cfile_path := file_name + %[5]c.go%[5]c%[3]c%[3]c%[4]c%[4]cfptr, err := os.OpenFile(file_path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)%[3]c%[4]c%[4]cif err != nil {%[3]c%[4]c%[4]c%[4]cos.Exit(1)%[3]c%[4]c%[4]c}%[3]c%[3]c%[4]c%[4]cs := %[5]c%[1]s%[5]c%[3]c%[4]c%[4]cw := bufio.NewWriter(fptr)%[3]c%[4]c%[4]cfmt.Fprintf(w, s, s, i, 10, 9, 34)%[3]c%[4]c%[4]cw.Flush()%[3]c%[3]c%[4]c%[4]cif err := fptr.Close(); err != nil {%[3]c%[4]c%[4]c%[4]cos.Exit(1)%[3]c%[4]c%[4]c}%[3]c%[3]c%[4]c%[4]ccompile_cmd := exec.Command(%[5]c/bin/sh%[5]c, %[5]c-c%[5]c, %[5]cgo build -o %[5]c + file_name + %[5]c %[5]c + file_path)%[3]c%[4]c%[4]ccompile_cmd.Start()%[3]c%[4]c%[4]ccompile_cmd.Wait()%[3]c%[3]c%[4]c%[4]cif i > 0 {%[3]c%[4]c%[4]c%[4]cexec_cmd := exec.Command(%[5]c./%[5]c + file_name)%[3]c%[4]c%[4]c%[4]cexec_cmd.Start()%[3]c%[4]c%[4]c%[4]cexec_cmd.Wait()%[3]c%[4]c%[4]c}%[3]c%[4]c}%[3]c}%[3]c"
+		s := "package main%[3]c%[3]cimport (%[3]c%[4]c%[5]cbufio%[5]c%[3]c%[4]c%[5]cfmt%[5]c%[3]c%[4]c%[5]cos%[5]c%[3]c%[4]c%[5]cos/exec%[5]c%[3]c%[4]c%[5]cstrconv%[5]c%[3]c)%[3]c%[3]cfunc main() {%[3]c%[4]ci := %[2]d%[3]c%[3]c%[4]cif _, err := os.Stat(%[5]cSully_5.go%[5]c); err == nil {%[3]c%[4]c%[4]ci -= 1%[3]c%[4]c}%[3]c%[3]c%[4]cif i >= 0 {%[3]c%[4]c%[4]cfile_name := %[5]cSully_%[5]c + strconv.Itoa(i)%[3]c%[4]c%[4]cfile_path := file_name + %[5]c.go%[5]c%[3]c%[3]c%[4]c%[4]cfptr, err := os.OpenFile(file_path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)%[3]c%[4]c%[4]cif err != nil {%[3]c%[4]c%[4]c%[4]cos.Exit(1)%[3]c%[4]c%[4]c}%[3]c%[3]c%[4]c%[4]cs := %[5]c%[1]s%[5]c%[3]c%[4]c%[4]cw := bufio.NewWriter(fptr)%[3]c%[4]c%[4]cfmt.Fprintf(w, s, s, i, 10, 9, 34)%[3]c%[4]c%[4]cw.Flush()%[3]c%[3]c%[4]c%[4]cif err := fptr.Close(); err != nil {%[3]c%[4]c%[4]c%[4]cos.Exit(1)%[3]c%[4]c%[4]c}%[3]c%[3]c%[4]c%[4]ccompile_cmd := exec.Command(%[5]c/bin/sh%[5]c, %[5]c-c%[5]c, %[5]cgo build -o %[5]c+file_name+%[5]c %[5]c+file_path)%[3]c%[4]c%[4]ccompile_cmd.Start()%[3]c%[4]c%[4]ccompile_cmd.Wait()%[3]c%[3]c%[4]c%[4]cif i > 0 {%[3]c%[4]c%[4]c%[4]cexec_cmd := exec.Command(%[5]c./%[5]c + file_name)%[3]c%[4]c%[4]c%[4]cexec_cmd.Start()%[3]c%[4]c%[4]c%[4]cexec_cmd.Wait()%[3]c%[4]c%[4]c}%[3]c%[4]c}%[3]c}%[3]c"
 		w := bufio.NewWriter(fptr)
 		fmt.Fprintf(w, s, s, i, 10, 9, 34)
 		w.Flush()
@@ -34,9 +34,7 @@ func main() {
 		}
 
 		compile_cmd := exec.Command("/bin/sh", "-c", "go build -o "+file_name+" "+file_path)
-		if err := compile_cmd.Start(); err != nil {
-			fmt.Println(err)
-		}
+		compile_cmd.Start()
 		compile_cmd.Wait()
 
 		if i > 0 {
